65a8e45edf2be71dc5d37593946bac23a329bb87
Timo Mayer <timo.mayer@virtual-identity.com>
2020-08-15 23:25:20 +0200

    feat: add commitlint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
+commitlint \
   | reviewdog -efm="%f:%l:%c: %m" \
       -name="linter-name (misspell)" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
